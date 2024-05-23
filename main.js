43b00f73c04fa8dc4a7b23e778f6a0db354f3614
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-23 21:44:32 +0530

    typed animated text is added

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var Typed = new Typed(".text", {
+        strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
