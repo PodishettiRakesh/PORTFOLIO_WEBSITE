421ee977dd5e574067ef816a48778baf7e90d3ad
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-05-29 15:37:20 +0530

    code modified

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed(".text", {
-        strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+        strings: ["Frontend Developer", "Full Stack developer", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
